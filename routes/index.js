3151d483ef6e12c51b3de44da6e913a2928f5afc
jjmirandaa86 <jjmirandaa86@gmail.com>
2024-10-31 12:58:06 +1100

    change message route /api

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router
 	.get("/", (req, res) => {
-		res.status(200).json({ error: "welcome api" });
+		res.status(200).json({ message: "welcome api" });
 	})
 	.use("/user", userRoutes);
 
